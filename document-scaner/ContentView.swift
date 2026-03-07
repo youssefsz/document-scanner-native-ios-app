25173eba5241b5af15115e3491d11d415955e58c
youssefsz <darhoumyi@gmail.com>
2026-03-07 18:22:11 +0100

    Initial Commit

---
+++ document-scaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  document-scaner
+//
+//  Created by Youssef Dhibi on 7/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ document-scaner/document_scanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  document_scanerApp.swift
+//  document-scaner
+//
+//  Created by Youssef Dhibi on 7/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct document_scanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
